69af532df1506de8e43a3283339385cdd72c3a40
unknown <riyannaffanusafara@gmail.com>
2024-09-23 19:29:15 +0700

    feat: complete code

--- case0-sample/main.go
+++ case0-sample/main.go
@@ -2,16 +2,20 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func CountLength(a, b float32) float32 {
-	// write your code below
+func CountLength(a, b float64) float64 {
+	c := math.Sqrt((a * a) + (b * b))
+	return c
 }
 
 func main() {
 	a := 2.0
 	b := 3.0
 
-	c := CountLength(float32(a), float32(b))
+	c := CountLength(float64(a), float64(b))
 	fmt.Println("length of c:", c)
 }
